8a048021a2bde57ddea5e584750b71bebfa459a5
IEVN1001-23002607 <angelfarnandez9@gmail.com>
2026-09-05 09:58:20 -0600

    operaciones basicas 05/09/26

---
+++ src/temasTS/01-tipos-bas.ts
@@ -0,0 +1,13 @@
+console.log("hola mundo")
+
+var num1=23
+let num2=25
+const num3=22
+
+
+let nombres:string
+let num:number
+let activos:boolean
+let suma:number=num1+num2+num3
+
+console.log("la suma es: + suma")
